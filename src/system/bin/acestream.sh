1394e40831b84729045003e2df7d73d720370864
alexelec <alexelecv5@gmail.com>
2018-02-02 18:39:50 +0200

    remove /system/bin/sh

--- src/system/bin/acestream.sh
+++ src/system/bin/acestream.sh
@@ -1,4 +1,4 @@
-#!/system/bin/sh
+#!/bin/sh
 
 export ANDROID_ROOT=/system
 export ANDROID_DATA=/system
@@ -7,14 +7,6 @@ export PYTHONHOME=/acestream.engine/python
 export PYTHONPATH=/acestream.engine/python/lib/python2.7/lib-dynload
 export PATH=$PYTHONHOME/bin:$PATH
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/acestream.engine/python/lib:/acestream.engine/python/lib/python2.7/lib-dynload
-
-mkdir -p /bin
-mkdir -p /sbin
-
-if [ ! -f /system/bin/complete.install ]; then
-  /system/bin/busybox --install -s
-  /system/bin/busybox touch /system/bin/complete.install
-fi
 
 cd /acestream.engine
 /acestream.engine/python/bin/python
