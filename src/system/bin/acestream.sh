c124ead49ed89047772dd0a1777df1ba4acced8d
alexelec <alexelecv5@gmail.com>
2018-02-02 18:43:52 +0200

    acestream.sh: fix PATH

--- src/system/bin/acestream.sh
+++ src/system/bin/acestream.sh
@@ -5,7 +5,7 @@ export ANDROID_DATA=/system
 export ANDROID_STORAGE=/media
 export PYTHONHOME=/acestream.engine/python
 export PYTHONPATH=/acestream.engine/python/lib/python2.7/lib-dynload
-export PATH=$PYTHONHOME/bin:$PATH
+export PATH=$PYTHONHOME/bin:$ANDROID_DATA/bin:$PATH
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/acestream.engine/python/lib:/acestream.engine/python/lib/python2.7/lib-dynload
 
 cd /acestream.engine
