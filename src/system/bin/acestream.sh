cb09e828de8671a706f4e6c1ea0556090d7d2832
alexelec <alexelecv5@gmail.com>
2018-03-03 15:32:50 +0200

    version: 3.1.24.2 (update webui)

--- src/system/bin/acestream.sh
+++ src/system/bin/acestream.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 
-export ANDROID_ROOT=/system
-export ANDROID_DATA=/system
-export ANDROID_STORAGE=/media
-export PYTHONHOME=/acestream.engine/python
-export PYTHONPATH=/acestream.engine/python/lib/python2.7/lib-dynload
-export PATH=$PYTHONHOME/bin:$ANDROID_DATA/bin:$PATH
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/acestream.engine/python/lib:/acestream.engine/python/lib/python2.7/lib-dynload
+ACEDIR='/acestream.engine'
 
-cd /acestream.engine
-/acestream.engine/python/bin/python
+export ANDROID_DATA=/system
+export ANDROID_ROOT=/system
+export ANDROID_STORAGE=/media
+export PYTHONHOME=$ACEDIR/python
+export PYTHONPATH=$ACEDIR/python/lib/python2.7/lib-dynload
+export PATH=$PATH:$ANDROID_ROOT/bin:$PYTHONHOME/bin
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$ACEDIR/python/lib:$ACEDIR/python/lib/python2.7/lib-dynload
+
+cd $ACEDIR
+$PYTHONHOME/bin/python
