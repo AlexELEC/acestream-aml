3688879ec0595e7fb33323190167e938fd03efc2
AlexELEC <alexelecv5@gmail.com>
2020-03-19 10:39:24 +0200

    fix shell in release.sh

--- release.sh
+++ release.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 ################################################################################
 #      This file is part of Alex@ELEC - http://www.alexelec.in.ua
 #      Copyright (C) 2011-present Alexandr Zuyev (alex@alexelec.in.ua)
